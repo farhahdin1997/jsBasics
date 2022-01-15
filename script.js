96cfca7c43c05fc95de4f49a6e27f799b17d6e46
farhah din <farhahdin97@hotmail.com>
2022-01-15 12:07:13 +0000

    aded

---
+++ script.js
@@ -0,0 +1,25 @@
+console.log ("hello world");
+/*
+function typeOf(input) {
+
+return typeof input;
+}
+
+console.log(typeOf("this is a string"));
+
+// testing the function
+console.log(typeOf(2));
+console.log(typeOf (true)); //expect boolean
+console.log(typeOf([0,1])); //expect an array
+console.log (typeOf (undefined)); 
+console.log(typeOf (undefined) == typeOff(null));*/
+
+
+function addNumber (number1, number2)
+{
+
+}
+
+console.log(addNumber(1, 2));
+console.log(addNumber ("four", "five"));
+
